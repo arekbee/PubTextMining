97a911cc85e51c88f69e669c6e0c3dede50b865b
Arkadiusz Beer <arekbee@pc9.home>
2015-05-14 01:21:05 +0200

    adding gitignore file and fix on process.r

--- src/Process.r
+++ src/Process.r
@@ -1,25 +1,30 @@
 
 library(tm)
+library(NLP)
 
-
-uri <-file.path("/pdfs/", "paper-to-review.pdf")
+uri <-file.path(".", "src/pdfs", "paper-to-review.pdf")
 pdf <- readPDF(control = list(text = "-layout"))(elem = list(uri = uri),
                                                  language = "en",
                                                  id = "id1")
 
 cor <- VCorpus(URISource(uri, mode = "", encoding = "UTF-8"),
-               readerControl = list(reader = readPDF(engine = "xpdf")))
+               readerControl = list(reader = readPDF(engine = "ghostscript")))
 
+inspect(cor)
 getTransformations()
-cor <- tm_map(news_corpus, content_transformer(tolower))
+cor <- tm_map(cor, content_transformer(tolower))
+
 cor <- tm_map(cor, removeNumbers)
 cor <- tm_map(cor, removePunctuation)
 cor <- tm_map(cor, stripWhitespace)
-cor <- tm_map(cor, removeSignature)
+
+#cor <- tm_map(cor, removeSignature)
+
+removeURL <- function(x) gsub("http[[:alnum:]]*", "", x)
+cor <- tm_map(cor, removeURL)
 
 
 stopwords("english")
-
 cor <- tm_map(cor, removeWords, stopwords("english"))
 
 
@@ -45,10 +50,10 @@ freq <- colSums(as.matrix(dtm))
 ord <- order(freq)
 freq[tail(ord)] # Most frequent terms
 
-
+tdm <- TermDocumentMatrix(cor,  control = list(wordLengths = c(1, Inf)))
 
 findFreqTerms(dtm, 5)
-findAssocs(dtm, "opec", 0.8)
+findAssocs(dtm, "sigma", 0.01)
 
 inspect(removeSparseTerms(dtm, 0.4))
 
@@ -59,3 +64,11 @@ dtms <- removeSparseTerms(dtm, 0.1)
 dim(dtms)
 inspect(dtms)
 freq <- colSums(as.matrix(dtms)
+                
+                
+df <- data.frame(term = names(freq), freq = freq)  
+
+library(ggplot2)
+ggplot(df, aes(x = term, y = freq)) + geom_bar(stat = "identity") +
+      xlab("Terms") + ylab("Count") + coord_flip()
+                
